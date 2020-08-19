feac52c849151c5f11294042324346ece9b89807
NamrataDevadiga <nd2561@columbia.edu>
2020-08-18 22:46:53 -0400

    uncommenting code

--- code.js
+++ code.js
@@ -11,22 +11,22 @@ jQuery(document).ready(function($) {
 		var path = window.location.pathname;
 		var page = path.split("/").pop();
 
-		// if (page == "index.html")
-		// {
-		// 	var elements = document.getElementsByClassName('txt-rotate');
-		// 	for (var i=0; i<elements.length; i++) {
-		// 	    var toRotate = elements[i].getAttribute('data-rotate');
-		// 	    var period = elements[i].getAttribute('data-period');
-		// 	    if (toRotate) {
-		// 	      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-		// 	   	}
-		// 	}
-		//   // INJECT CSS
-		//   	var css = document.createElement("style");
-		//   	css.type = "text/css";
-		//   	css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-		//   	document.body.appendChild(css);
-		// }
+		if (page == "index.html")
+		{
+			var elements = document.getElementsByClassName('txt-rotate');
+			for (var i=0; i<elements.length; i++) {
+			    var toRotate = elements[i].getAttribute('data-rotate');
+			    var period = elements[i].getAttribute('data-period');
+			    if (toRotate) {
+			      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+			   	}
+			}
+		  // INJECT CSS
+		  	var css = document.createElement("style");
+		  	css.type = "text/css";
+		  	css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+		  	document.body.appendChild(css);
+		}
 
 	    if (page == "contact.html")
 	    {
@@ -101,6 +101,7 @@ jQuery(document).ready(function($) {
 
 		if (page == "index.html")
 		{
+
 			var elements = document.getElementsByClassName('txt-rotate');
 			for (var i=0; i<elements.length; i++) {
 			    var toRotate = elements[i].getAttribute('data-rotate');

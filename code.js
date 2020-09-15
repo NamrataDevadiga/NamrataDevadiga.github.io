53cdc7afaf31afcfa4889808d16435829cd2bc84
NamrataDevadiga <nd2561@columbia.edu>
2020-09-14 21:28:38 -0400

    Update code.js

--- code.js
+++ code.js
@@ -93,53 +93,53 @@ jQuery(document).ready(function($) {
   	}, delta);
 	};
 
-	$(window).on('load', function(){
-		console.log("Inside on load");
-		var ratio = window.innerWidth/window.innerHeight;
-		var path = window.location.pathname;
-		var page = path.split("/").pop();
+	// $(window).on('load', function(){
+	// 	console.log("Inside on load");
+	// 	var ratio = window.innerWidth/window.innerHeight;
+	// 	var path = window.location.pathname;
+	// 	var page = path.split("/").pop();
 
-		if (page == "index.html" || page == "")
-		{
-			var elements = document.getElementsByClassName('txt-rotate');
-			for (var i=0; i<elements.length; i++) {
-			    var toRotate = elements[i].getAttribute('data-rotate');
-			    var period = elements[i].getAttribute('data-period');
-			    if (toRotate) {
-			      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-			   	}
-			}
-		  // INJECT CSS
-		  	var css = document.createElement("style");
-		  	css.type = "text/css";
-		  	css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-		  	document.body.appendChild(css);
-		}
+	// 	if (page == "index.html" || page == "")
+	// 	{
+	// 		var elements = document.getElementsByClassName('txt-rotate');
+	// 		for (var i=0; i<elements.length; i++) {
+	// 		    var toRotate = elements[i].getAttribute('data-rotate');
+	// 		    var period = elements[i].getAttribute('data-period');
+	// 		    if (toRotate) {
+	// 		      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+	// 		   	}
+	// 		}
+	// 	  // INJECT CSS
+	// 	  	var css = document.createElement("style");
+	// 	  	css.type = "text/css";
+	// 	  	css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+	// 	  	document.body.appendChild(css);
+	// 	}
 		
-		if (page == "contact.html")
-	    {
-	    	$('.content') .hide()
-	    	if (window.innerWidth >= 1300 && window.innerHeight >= 700 && ratio >= 1.75)
-	    	{
-	    		$(".contact-small").hide();
-	    		$(".contact-medium").hide();
-	    		$(".contact-large").show();
-	    	}
-	    	else if(window.innerWidth <= 590)
-	    	{
-	    		$(".contact-large").hide();
-	    		$(".contact-small").show();
-	    		$(".contact-medium").hide();
-	    	}
-	    	else
-	    	{
-	    		$(".contact-large").hide();
-	    		$(".contact-small").hide();
-	    		$(".contact-medium").show();
-			}
-			$('.content').show();
-	    }
-	});
+	// 	if (page == "contact.html")
+	//     {
+	//     	$('.content') .hide()
+	//     	if (window.innerWidth >= 1300 && window.innerHeight >= 700 && ratio >= 1.75)
+	//     	{
+	//     		$(".contact-small").hide();
+	//     		$(".contact-medium").hide();
+	//     		$(".contact-large").show();
+	//     	}
+	//     	else if(window.innerWidth <= 590)
+	//     	{
+	//     		$(".contact-large").hide();
+	//     		$(".contact-small").show();
+	//     		$(".contact-medium").hide();
+	//     	}
+	//     	else
+	//     	{
+	//     		$(".contact-large").hide();
+	//     		$(".contact-small").hide();
+	//     		$(".contact-medium").show();
+	// 		}
+	// 		$('.content').show();
+	//     }
+	// });
 
 	$(window).resize(function(){
 		var ratio = window.innerWidth/window.innerHeight;
